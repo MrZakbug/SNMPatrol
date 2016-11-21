c7aa77647cfdbbdd3f292e8330633483dc6a7851
MrZakbug <mrzakbug@gmail.com>
2016-11-21 13:02:04 +0100

    Working delete function

--- Bin/process_data.py
+++ Bin/process_data.py
@@ -26,15 +26,16 @@ def import_data(table_name, *args):
 def delete_data(table_name, *args):
     seq = ("'", "'")
     if len(args) == 0:
-        db.execute('delete * from {}'.format(table_name))
+        db.execute('delete from {}'.format(table_name))
     if len(args) in [1, 2]:
-        db.execute('delete * from {} where date between {} and {}'
+        db.execute('delete from {} where date between {} and {}'
                    .format(table_name, args[0].join(seq), args[1].join(seq)))
     if len(args) in [3, 4]:
-        db.execute('delete * from {} where (date between {} and {}) and (datetime between {} and {})'
+        db.execute('delete from {} where (date between {} and {}) and (datetime between {} and {})'
                    .format(table_name, args[0].join(seq), args[1].join(seq), args[2].join(seq),
                            args[3].join(seq)))
     db.commit()
+
 
 def db_to_json(table_name, *args):
     seq = ("'", "'")
@@ -72,6 +73,7 @@ def min_value(values):
 
 
 if __name__ == '__main__':
+    '''
     print(import_data('ifInOctets1', '2016/11/14', '2016/11/14'))
     print(avg_value(import_data('ifInOctets1', '2016/11/14', '2016/11/14')))
 
@@ -79,3 +81,5 @@ if __name__ == '__main__':
     print(avg_value(import_data('ifInOctets1', '2016/11/14', '2016/11/14', '12:10:01', '12:11:55')))
 
     db_to_json('ifInOctets1', '2016/11/14', '2016/11/14', '12:10:01', '12:14:05')
+    '''
+    delete_data('ifInOctets1', '2016/11/14', '2016/11/14', '12:10:01', '12:14:30')
