9b37542f5d77620cfa411c08acefd23fb9c90bbd
MrZakbug <mrzakbug@gmail.com>
2016-11-22 09:50:16 +0100

    Finished DB optimization module for last week, last month and previous months optimization

--- Bin/db_optimization.py
+++ Bin/db_optimization.py
@@ -7,7 +7,7 @@ db = sqlite3.connect('SNMPatrol.db')
 
 def last_week_optimization(table_name):
     today = datetime.date.today()
-    for day in range(7, 8):
+    for day in range(7, 15):
         for hour in range(0, 24):
             if hour < 10:
                 current_hour = '0' + str(hour) + ':00:00'
@@ -24,22 +24,41 @@ def last_week_optimization(table_name):
             db.commit()
 
 
-def last_month_optimization():
-    pass
-    # for each day
-        # for each 3 hours
-            # avg the values
-            # delete all the values in this time stamp
-            # insert avg value with specific time
+def last_month_optimization(table_name):
+    today = datetime.date.today()
+    for day in range(15, 31):
+        for hour in range(0, 24, 3):
+            if hour < 7:
+                current_hour = '0' + str(hour) + ':00:00'
+                next_hour = '0' + str(hour + 2) + ':59:59'
+            elif hour == 9:
+                current_hour = '0' + str(hour) + ':00:00'
+                next_hour = str(hour + 2) + ':59:59'
+            else:
+                current_hour = str(hour) + ':00:00'
+                next_hour = str(hour + 2) + ':59:59'
+
+            day_to_optimize = today - datetime.timedelta(days=day)
+            avg = avg_value(import_data(table_name, day_to_optimize, day_to_optimize, current_hour, next_hour))
+            delete_data(table_name, day_to_optimize, day_to_optimize, current_hour, next_hour)
+            db.execute('INSERT INTO {} (DATE, DATETIME, VALUE) VALUES(?, ?, ? )'.format(table_name),
+                       (day_to_optimize, current_hour, avg))
+            db.commit()
 
 
-def previous_months_optimization():
-    pass
-    # for each day
-        # avg the values
-        # delete all the values in this time stamp
-        # insert avg value with specific time
+def previous_months_optimization(table_name):
+    today = datetime.date.today()
+    for day in range(31, 91):
+        day_to_optimize = today - datetime.timedelta(days=day)
+        avg = avg_value(import_data(table_name, day_to_optimize, day_to_optimize))
+        delete_data(table_name, day_to_optimize, day_to_optimize)
+        db.execute('INSERT INTO {} (DATE, DATETIME, VALUE) VALUES(?, ?, ? )'.format(table_name),
+                   (day_to_optimize, '12:00:00', avg))
+        db.commit()
+
 
 if __name__ == '__main__':
     db = sqlite3.connect('SNMPatrol.db')
+    # previous_months_optimization('hrDeviceDescr196608')
+    # last_month_optimization('hrDeviceDescr196608')
     last_week_optimization('hrDeviceDescr196608')
