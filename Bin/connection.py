406cb6302d81dcd7027f8c4d67267acde019148f
MrZakbug <mrzakbug@gmail.com>
2016-11-26 21:55:02 +0100

    Switched to pySNMP

--- Bin/connection.py
+++ Bin/connection.py
@@ -1,3 +1,6 @@
+from pysnmp.hlapi import *
+
+
 def ping(host):
     """
     Returns True if host responds to a ping request
@@ -11,11 +14,27 @@ def ping(host):
     # Ping
     return os.system("ping " + ping_str + " " + host) == 0
 
-def snmp_walk(mib,hostname = '0.0.0.0', community = 'public', version = '1')
-    from easysnmp import snmp_get
 
-    snmp_get({}, hostname={}, community={}, version={}.format(mib, hostname, community, version))
+def snmp_walk():
+
+    error_indication, error_status, error_index, var_binds = next(
+        getCmd(SnmpEngine(),
+               CommunityData('public', mpModel=0),
+               UdpTransportTarget(('demo.snmplabs.com', 161)),
+               ContextData(),
+               ObjectType(ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)))
+    )
+
+    if error_indication:
+        print(error_indication)
+    elif error_status:
+        print('%s at %s' % (error_status.prettyPrint(),
+                            error_index and var_binds[int(error_index) - 1][0] or '?'))
+    else:
+        for varBind in var_binds:
+            print(' = '.join([x.prettyPrint() for x in varBind]))
 
 
 if __name__ == '__main__':
-    print(ping('10.5.30.44'))
+    print(ping('8.8.8.8'))
+    print(snmp_walk())
