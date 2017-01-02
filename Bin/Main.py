34f1e5b8a0585556c1144dc93c3ee5cb7b9d7f3a
MrZakbug <mrzakbug@gmail.com>
2017-01-02 22:46:53 +0100

    Adding required files, config file and working on main function

--- Bin/Main.py
+++ Bin/Main.py
@@ -1,10 +1,13 @@
-import device_class
+from device_class import Device
+import json
+
+with open('required\\Config.json', 'r') as data_file:
+    config = json.load(data_file)
+print(config["Devices"])
+devices = []
+for device_config in config["Devices"]:
+    devices.append(Device(device_config["Device Name"], device_config["Device IP Address"], device_config["Device DNS"],
+                          device_config["Device OS"],device_config["Device Type"]))
 
 
-with open('required\\Config.txt', 'r') as inf:
-    config = eval(inf.read())
-
-for device in config['Devices']:
-    device['Device Name'] = device_class.Device(device['Device Name'], device['Device IP Address'],
-                                                device['Device DNS'], device['Device OS'], device['Device Type'])
-
+devices[0].ping()
