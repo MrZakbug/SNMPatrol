1047b4bca395e77b7503a95afecd1d35cf31bae8
MrZakbug <mrzakbug@gmail.com>
2017-01-03 21:17:52 +0100

    Deleted unnecessary files, minor improvements to main

--- Bin/Main.py
+++ Bin/Main.py
@@ -2,9 +2,11 @@ import device_class
 import json
 import multiprocessing
 
+
 def create_device_instances():
-    with open('required\\Config.json', 'r') as data_file:
-        config = json.load(data_file)
+    data_file = open('required\\Config.json', 'r')
+    config = json.load(data_file)
+    data_file.close()
     devices = []
     for device_config in config["Devices"]:
         devices.append(device_class.Device(device_config["Device Name"], device_config["Device IP Address"],
