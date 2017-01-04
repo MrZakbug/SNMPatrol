48bc4fa40d33ec7d44b7b12408382e14d03bca09
MrZakbug <mrzakbug@gmail.com>
2017-01-04 15:36:12 +0100

    Main module now calls for SNMP walks of every device correctly, optimizes each table of db every night, and supports multiprocessing

--- Bin/Main.py
+++ Bin/Main.py
@@ -1,27 +1,63 @@
 import device_class
+import db_optimization
 import json
 import multiprocessing
+from time import sleep, strftime
 
 
 def create_device_instances():
+    """
+    Creates a list of device instances stated in config.json file
+    """
     data_file = open('required\\Config.json', 'r')
     config = json.load(data_file)
     data_file.close()
-    devices = []
+    devices_list = []
     for device_config in config["Devices"]:
-        devices.append(device_class.Device(device_config["Device Name"], device_config["Device IP Address"],
-                                           device_config["Device DNS"], device_config["Device OS"],
-                                           device_config["Device Type"]))
-    return devices
+        devices_list.append(device_class.Device(device_config["Device Name"], device_config["Device IP Address"],
+                                                device_config["Device DNS"], device_config["Device OS"],
+                                                device_config["Device Type"]))
+    return devices_list
 
 
-def main(device):
-    print(device.name)
-    device.snmp_walk()
-    print(range(1, 50))
+def db_optimization(device_instance):
+    """
+    Optimizes each table of each device
+    """
+    for table in device_instance.device.tables:
+        db_optimization.data_base_optimization(table)
+
+
+def main(device_instance):
+    """
+    Performs SNMP walk for each device and each device's OID stated in neededOIDs-????.txt file, checks for
+    warnings/errors and sends alerts mail and/or imputs the data inot the DB
+    """
+    print(device_instance.name)
+    device_instance.snmp_walk()
 
 if __name__ == '__main__':
-    for device in create_device_instances():
-        p = multiprocessing.Process(target=main(device))
-        p.start()
+    devices = create_device_instances()
+    today = strftime("%Y-%m-%d", )
+    optimized_today = False
+    while True:
+        """
+        For every device in devices list starts a process of main function. If time is between 00:00;00 and 01:00:00 and
+        DB was not optimized this day starts processes of optimization for each device.
+        """
+        for device in devices:
+            process = multiprocessing.Process(target=main, args=(device,))
+            process.start()
 
+        sleep(300)
+
+        if '00' < strftime("%H", ) < '01' and not optimized_today:
+            for device in devices:
+                optimization = multiprocessing.Process(target=db_optimization, args=(device,))
+                optimization.start()
+            optimized_today = True
+
+        if strftime("%Y-%m-%d", ) > today:
+            # Changes the status of optimization when day change and today to today's date for further checks
+            today = strftime("%Y-%m-%d", )
+            optimized_today = False
